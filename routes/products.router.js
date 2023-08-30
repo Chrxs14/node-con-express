ae84d23954a21e300aeb6d8983f96c91ed30345c
Chrxs14 <trabajo.ochoachristopher@gmail.com>
2023-08-30 08:11:36 -0500

    ...

--- routes/products.router.js
+++ routes/products.router.js
@@ -17,20 +17,26 @@ router.get('/filter', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await services.getProductById(parseInt(id));
+  const product = await services.getProductById(id);
   res.json(product);
 });
 
 router.patch('/:id', async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = await services.updateProductById(parseInt(id), body);
-  res.json(product);
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await services.updateProductById(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: 'Product not found',
+    });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await services.deleteProductById(parseInt(id));
+  const product = await services.deleteProductById(id);
   res.json(product);
 });
 
@@ -38,19 +44,6 @@ router.post('/', async (req, res) => {
   const body = req.body;
   const newProduct = services.createProduct(body);
   res.status(201).json(newProduct);
-});
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  const product = services.updateProductById(parseInt(id), body);
-  res.json(product);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const rta = services.deleteProductById(parseInt(id));
-  res.json(rta);
 });
 
 module.exports = router;
