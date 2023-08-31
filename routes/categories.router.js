82eb47b5f04b38bb541dee36200a193dcd5babd5
Cesar Tipan <cesar.tipan@bitekso.com>
2023-08-31 10:26:51 -0500

    categorias 2.0

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -18,7 +18,15 @@ router.get('/', async (req, res) => {
 router.get(
   '/:id',
   validatorHandler(getCategorySchema, 'params'),
-  async (req, res, next) => {},
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await services.getCategoriesById(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  },
 );
 
 router.get('/:id/products/:productId', (req, res) => {
@@ -27,6 +35,38 @@ router.get('/:id/products/:productId', (req, res) => {
     name: `Producto ${productId} de la categoria ${id}`,
     price: 1000,
   });
+});
+
+router.post(
+  '/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newCategory = await services.createCategories(body);
+    res.status(201).json(newCategory);
+  },
+);
+
+router.patch(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await services.updateCategoriesById(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await services.deleteCategoryById(id);
+  res.json(rta);
 });
 
 module.exports = router;
